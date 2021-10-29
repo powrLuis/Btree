d27f57ab72d9280e32d089e477a4a98f3b7fe4d5
Luis Ronald Istaña Chipana <l.ronald.ic@outlook.com>
2021-10-28 23:50:03 -0500

    muestra de resultados

--- Btree.cpp
+++ Btree.cpp
@@ -223,13 +223,27 @@ int main()
 	{
 		bt.insert(v);
 	}
-	bt.find('5');
-	bt.find('m');
 	bt.print();
+
+	std::cout << "======busqueda======="<<std::endl;
+	std::string consulta = "5my23";
+	for (auto c : consulta)
+	{
+		if (bt.find(c))
+		{
+			std::cout << "valor " << c << " se encontro"<<std::endl;
+		}
+		else
+		{
+			std::cout << "valor " << c << " no se encontro"<<std::endl;
+		}
+	}
+	std::cout << "==busqueda por rango===="<<std::endl;
 	auto r = bt.find_range('l', 't');
+	std::cout << "valores entre l y t"<<std::endl;
 	for (auto v:r)
 	{
-		std::cout << (char)v << ", ";
+		std::cout << (char)v << " ";
 	}
 
 }
